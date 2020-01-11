0928e1612bfbdc498ca38c211f15df805a092836
Nico Mayer <MayerNico@outlook.de>
2020-01-11 14:59:43 +0100

    Expand the snake test

--- tests/snake_test.cpp
+++ tests/snake_test.cpp
@@ -3,6 +3,11 @@
 
 TEST_CASE("Create a new snake", "[snake]") {
 
-    Snake(Snake::UP, {{0, 0}, {1, 0}, {2, 0}});
+    Snake snake(Snake::UP, {{0, 0}, {1, 0}, {2, 0}});
 
+    REQUIRE(snake.is_on_pos(0,0));
+    REQUIRE(snake.is_on_pos(1,0));
+    REQUIRE(snake.is_on_pos(2,0));
+    
+    REQUIRE(snake.get_direction() == Snake::UP);
 }
