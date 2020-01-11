037eb797bfafdc3d6ca365daa0773d459a9f29a5
Nico Mayer <MayerNico@outlook.de>
2020-01-11 17:53:38 +0100

    Modify the snake tests

--- tests/snake_test.cpp
+++ tests/snake_test.cpp
@@ -1,14 +1,56 @@
 #include "catch.hpp"
 #include "snake.hpp"
+#include "point.hpp"
 
-TEST_CASE("Create a new snake", "[snake]") {
+TEST_CASE( "snake can be created", "[snake]" ) {
 
-    Snake snake(Snake::UP, {{0, 0}, {1, 0}, {2, 0}});
+    Snake snake(Snake::UP, {{x:0, y:0}, {x:1, y:0}, {x:2, y:0}});
 
-    REQUIRE(snake.is_on_pos(Point{0,0}));
-    REQUIRE(snake.is_on_pos(Point{1,0}));
-    REQUIRE(snake.is_on_pos(Point{2,0}));
-    REQUIRE(snake.get_head_pos() == Point{0, 0});
+    REQUIRE( snake.is_on_pos(Point{x:0, y:0}) );
+    REQUIRE( snake.is_on_pos(Point{x:1, y:0}) );
+    REQUIRE( snake.is_on_pos(Point{x:2, y:0}) );
+
+    REQUIRE( snake.get_head_pos() == Point{x:0, y:0} );
     
-    REQUIRE(snake.get_direction() == Snake::UP);
+    REQUIRE( snake.get_direction() == Snake::UP );
 }
+
+TEST_CASE( "snake can be move", "[snake]" ) {
+
+    Snake snake(Snake::UP, {{x:10, y:10}});
+
+    REQUIRE( snake.is_on_pos(Point{x:10, y:10}) );
+
+    SECTION( "snake can be move up") {
+
+        snake.change_direction(Snake::UP);
+            
+        REQUIRE( snake.is_on_pos(Point{x:10, y:9}) );
+
+    }
+
+    SECTION( "snake can be move down" ) {
+
+        snake.change_direction(Snake::DOWN);
+
+        REQUIRE( snake.is_on_pos(Point{x:10, y:11}) );
+
+    }
+
+    SECTION( "snake can be move right" ) {
+
+        snake.change_direction(Snake::RIGHT);
+
+        REQUIRE( snake.is_on_pos(Point{x:9, y:10}) );
+
+    }
+
+    SECTION( "snake can be move left" ) {
+        
+        snake.change_direction(Snake::LEFT);
+        
+        REQUIRE( snake.is_on_pos(Point{x:11, y:10}) );
+    }
+
+}
+         
