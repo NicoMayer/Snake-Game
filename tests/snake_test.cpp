1b237f4b300bb79168740593c6fa9c2d2ce3b4fc
Nico Mayer <MayerNico@outlook.de>
2020-01-11 15:40:10 +0100

    Add a test for Snake::get_head_pos()

--- tests/snake_test.cpp
+++ tests/snake_test.cpp
@@ -8,6 +8,7 @@ TEST_CASE("Create a new snake", "[snake]") {
     REQUIRE(snake.is_on_pos(0,0));
     REQUIRE(snake.is_on_pos(1,0));
     REQUIRE(snake.is_on_pos(2,0));
+    REQUIRE(snake.get_head_pos() == {0, 0});
     
     REQUIRE(snake.get_direction() == Snake::UP);
 }
