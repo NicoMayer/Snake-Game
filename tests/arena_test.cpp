8410ae89244f6987f09ed40ddc0bd4386df08776
Nico Mayer <MayerNico@outlook.de>
2020-01-12 14:24:15 +0100

    Fix the arena tests
    Make_shared can not call ctors with initializer_list. Instead, snake shared_ptr are created with new.

--- tests/arena_test.cpp
+++ tests/arena_test.cpp
@@ -1,8 +1,9 @@
+#include <memory>
 #include "catch.hpp"
 #include "arena.hpp"
 #include "snake.hpp"
 
-TEST_CASE( "arena can move the snake", [arena]){
+TEST_CASE( "arena can move the snake", "[arena]"){
 
    
     SECTION( "move_snake without a snake return false" ) {
@@ -12,7 +13,7 @@ TEST_CASE( "arena can move the snake", [arena]){
 
     SECTION( "move_snake returns false if the head leaves the arena" ) {
         
-        auto snake = make_shared<Snake>(Snake::UP, {{10, 1}, {10, 2}, {10, 3}});
+        auto snake = std::shared_ptr<Snake>(new Snake(Snake::UP, {{10, 1}, {10, 2}, {10, 3}}));
         
         Arena arena(snake, 16, 16);
 
@@ -22,7 +23,7 @@ TEST_CASE( "arena can move the snake", [arena]){
 
     SECTION( "move_snake returns false if the head leaves not the arena" ) {
 
-        auto snake = make_shared<Snake>(Snake::RIGHT, {{10, 10}, {11,10}});
+        auto snake = std::shared_ptr<Snake>(new Snake(Snake::RIGHT, {{10, 10}, {11,10}}));
 
         Arena arena(snake, 16, 16);
 
@@ -32,10 +33,10 @@ TEST_CASE( "arena can move the snake", [arena]){
 
     SECTION( "move_snake returns false if snake collides with itself" ) {
 
-        auto snake = make_shared<Snake>
-                        (Snake::DOWN, {{10, 5}, {9, 5}, {9, 6}, {10, 6}, {11, 6}}); 
+        auto snake = std::shared_ptr<Snake>
+                        (new Snake(Snake::DOWN, {{10, 5}, {9, 5}, {9, 6}, {10, 6}, {11, 6}})); 
 
-        Arena arean(snake, 16, 16);
+        Arena arena(snake, 16, 16);
 
         REQUIRE( arena.move_snake() == false);
     }
