cf93f7520a206335b5c6d72f015ce768f4bf603e
Nico Mayer <MayerNico@outlook.de>
2020-01-12 14:18:00 +0100

    Implement the new snake functions. Snake can now move.

--- source/snake.cpp
+++ source/snake.cpp
@@ -10,8 +10,8 @@ bool Snake::is_on_pos(Point pos) const {
 
     bool is_on_position = false;
    
-    for(auto snake_cell : body) {
-        if (pos == snake_cell) {
+    for(auto snake_cell_pos : body) {
+        if (snake_cell_pos.equal(pos)) {
             is_on_position = true;
         }
     } 
@@ -24,9 +24,45 @@ Snake::Direction Snake::get_direction() const {
 
 Point Snake::get_head_pos() const {
     if (body.size() > 0) {
-        return body[0];
+        return body.front();
     }
     else {
         return Point{0,0};
     }
 }
+
+void Snake::change_direction(Snake::Direction dir) {
+
+    direction = dir;
+}
+
+bool Snake::move() {
+
+    Point new_snake_head = body.front();
+
+    switch(direction) {
+        case Snake::UP:
+            new_snake_head.add(0, -1);
+            break;
+        case Snake::DOWN:
+            new_snake_head.add(0, 1);
+            break;
+        case Snake::LEFT:
+            new_snake_head.add(-1, 0);
+            break;
+        case Snake::RIGHT:
+            new_snake_head.add(1, 0);
+            break;
+    }
+
+    if (!is_on_pos(new_snake_head)) {
+        body.pop_back();
+        body.push_front(new_snake_head);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+         
+    
